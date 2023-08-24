b5a438a8d60ae19cf6ee405e3e782876f08d271f
Eunyul Kim <e7yxi77@gmail.com>
2023-08-24 18:20:25 +0900

    hello

---
+++ week1/test.py
@@ -0,0 +1,9 @@
+name = "Eunyul"
+print(name)
+"Hello"
+print("Hello")
+"Hello world"
+print("Hello","World")
+print("Hello")
+print("World")
+print("Because I could not stop for death, He kindly stopped for me; The carriage held but just ourselves And Immortality")
