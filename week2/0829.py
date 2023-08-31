d9464271e50f06321c386cd2a3dc7e8919c8bd4b
Eunyul Kim <e7yxi77@gmail.com>
2023-08-31 17:07:10 +0900

    0829 수업 끝

---
+++ week2/0829.py
@@ -0,0 +1,66 @@
+print("intel core i7 12700F")
+"""
+print("intel core i7 12700F")
+print("intel core i7 12700F")
+print("intel core i7 12700F")
+print("intel core i7 12700F")
+print("intel core i7 12700F")
+print("intel core i7 12700F")
+print("intel core i7 12700F")
+print("intel core i7 12700F")
+print("intel core i7 12700F")
+print("intel core i7 12700F")
+print("intel core i7 12700F")
+"""
+# print("intel core i7 12700F")
+a = 100
+if a == 0:
+    print(1)
+else:
+    print(0)
+
+# nummber => integer = int (정수형) = 1,2,3 ...
+# float(실수형) = 1.0, 2.0, 3.0 ...
+
+a = float("123123")
+print(a)
+
+b = int("123123")
+print(b)
+
+c = 10
+d = 3
+print(c / d) #devide
+print(c // d) #Geteilte Zahl
+print(c % d) #rest
+print(10 ** 10) #Fache...
+
+e = int("100")
+f = int("3") 
+
+print(e / f) 
+print(e // f)
+print(e % f)
+
+GB16 = 16000
+
+# print(16*PB) 
+kb = 1000
+mb = 1000*kb
+gb = 1000*mb
+tb = 1000*gb
+pb = 1000*tb
+eb = 1000*pb
+zb = 1000*eb
+yb = 1000*zb
+
+print(yb*16)
+
+
+"""
+CPU: intel core i7 12700F
+GPU: Colorfull nVidia RTX 3060
+RAM:KLEVV 3200 LED lightstrip 16GB
+MAINBOARD: ASUS H610 M-K 
+CPU COOLER: NZXT KRAKEN Z63
+"""
