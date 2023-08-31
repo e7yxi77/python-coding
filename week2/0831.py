4b67a741ffd93c0502acfe65c27f6a7fbe0949be
Eunyul Kim <e7yxi77@gmail.com>
2023-08-31 18:41:51 +0900

    0831수업 끝

---
+++ week2/0831.py
@@ -0,0 +1,83 @@
+# a, b, c = 1, 1, 1
+# print(a)
+# print(b)
+# print(c)
+# x = 1 
+# del x
+
+# x = None
+# print(x)
+# x = 1
+# print(x)
+
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+# a = 10 
+# a + 20
+# #30
+# a
+# #10
+
+# a = 10
+# a = a + 20 # a와 20을 더한 후 결과를 다 a에 저장
+# a
+# #30
+
+# a = 10 
+# a += 20 # a와 20을 더한 후  결과를 다시 a에 저장
+# a 
+# #30
+
+# d += 10 # 만들지 않은 변수
+
+# x = -10
+# +x
+# #-10
+# -x
+# #10
+
+# a= input("이름:")
+# print(a)
+
+# b= input("전화번호:")
+# print(b)
+
+# c= input("주소:")
+# print(c)
+
+# d= input("이메일:")
+# print(d)
+
+# x=input()
+# y=input()
+
+# x=int(x)
+# y=int(y)
+# print(x+y)
+
+# y=input()
+# print(y)
+
+
+# a=input()
+# a = int(a)
+# print=(a)
+
+# a=input()
+# a = float(a)
+# print(a)
+
+# a=input()
+# b=input()
+# a = int(a)
+# b = int(b)
+# print(a)
+# print(b)
+
+a=input()
+b=input()
+print(b)
+print(a)
