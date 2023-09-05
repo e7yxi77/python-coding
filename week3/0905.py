5e85306cf12bab2a8e5820db821654754de2ab21
Eunyul Kim <e7yxi77@gmail.com>
2023-09-05 18:48:43 +0900

    0905수업 끝

--- week3/0905.py
+++ week3/0905.py
@@ -1,7 +1,38 @@
-a = "'hi'"
+# a = "'hi'"
+# print(a)
+# b = '"hi"'
+# print(b)
+# print("\"!@#$%^&*()'")
+# print("\"C:\Download\\'hello'.py\"")
+# print("print(\"Hello\\nWorld\")")
+# c,d,e = input().split(" ")
+# print(c)
+# print(d)
+# print(e)
+
+# a=50
+# b=100
+# c=None
+
+# c,d,e = map(int,input().split(" "))
+
+# print(type(c))
+
+# a, b, c, d = map(int,input().split(" "))
+# print(int((a+b+c+d)/4))
+
+# a=10
+# b=20
+# c=30
+# print(a,b,c)
+
+# a = float(input())
+# print(a)
+# print(a)
+# print(a)
+
+a=1
+b=2
+a,b=map(int,input().split())
 print(a)
-b = '"hi"'
 print(b)
-print("\"!@#$%^&*()'")
-print("\"C:\Download\\'hello'.py\"")
-print("print(\"Hello\\nWorld\")")
