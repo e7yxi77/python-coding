8126da70c0dc01082dc90f56df550a955f6e90d6
Eunyul Kim <e7yxi77@gmail.com>
2023-09-21 19:44:44 +0900

    0921수업끝

---
+++ 0921.py
@@ -0,0 +1,69 @@
+# a=input()
+# a=int(a)
+# print(-a)
+
+# a,b=map(int,input().split())
+# c=a-b
+# print(c)
+
+# a,b=map(float,input().split())
+# c=a*b
+# print(c)
+
+# a,b=input().split()
+# print(a*int(b))
+
+# a=input()
+# b=input()
+# print(int(a)*b)
+
+# a,b=map(int,input().split())
+# c=a**b
+# print(c)
+
+# a,b=map(float,input().split())
+# c=a**b
+# print(c)
+
+# a,b=map(int,input().split())
+# c=a%b
+# print(c)
+
+# a,b=map(float,input().split())
+# c=a/b
+# print(f"{c:0.3f}"
+
+# a,b=map(int,input().split())
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a%b)
+# print(f"{a/b:0.2f}")
+
+# a,b,e=map(int,input().split())
+# c=(a+b+e)
+# d=f"{c/3:0.2f}"
+# print(c,d)
+
+# a,b=map(int,input().split())
+# print(a<b)
+
+# a,b=map(int,input().split())
+# print(a==b)
+
+# a,b=map(int,input().split())
+# print(a<=b)
+
+# a,b=map(int,input().split())
+# print(a!=b)
+
+# a=input()
+# a=int(a)
+# print(bool(a))
+
+a=bool(int(input()))
+print(not a)
+
+
+
