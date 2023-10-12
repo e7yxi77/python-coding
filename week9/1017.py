43a7df20a989bb9545bd07135706e99b9b7975c6
Eunyul Kim <e7yxi77@gmail.com>
2023-10-12 22:50:51 +0900

    1012수업 끝

---
+++ week9/1017.py
@@ -0,0 +1,14 @@
+# # 문제 1
+# a = [1,2,3,[7,6,5]]
+# # 결과값
+# a= [1,2,3,[5,6,7],8,9,10] # append랑, reverse 또는 reversed를 통해서 구현
+
+a=[1,2,3,[7,6,5]]
+
+a.append(8)
+a.append(9)
+a.append(10)
+a[3].reverse()
+
+print(a)
+
