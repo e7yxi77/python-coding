e703baa5bb19b021ca3e644db33a8820e0845216
Eunyul Kim <e7yxi77@gmail.com>
2023-11-21 08:54:17 +0900

    dlkjsflkj

--- week1/test.py
+++ week1/test.py
@@ -15,39 +15,32 @@ import turtle
 import time
 import random
 
-delay = 0.1
+import turtle
+import time
+import random
 
-# Score
+delay= 0.1
 score = 0
 high_score = 0
-
-# Set up the screen
-wn = turtle.Screen()
-wn.title("Ajfon 14 ")
+wn=turtle.Screen()
+wn.title("snakegame exyx")
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
-wn.tracer(0) # Turns off the screen updates
-
-# Snake head
-head = turtle.Turtle()
+wn.tracer(0)
+head=turtle.Turtle()
 head.speed(1)
 head.shape("square")
 head.color("black")
 head.penup()
 head.goto(0,0)
-head.direction = "stop"
-
-# Snake food
-food = turtle.Turtle()
+head.direction="stop"
+food=turtle.Turtle()
 food.speed(0)
-food.shape("circle")
-food.color("red")
+food.shape("square")
+food.shape("yellow")
 food.penup()
 food.goto(0,100)
-
 segments = []
-
-# Pen
 pen = turtle.Turtle()
 pen.speed(0)
 pen.shape("square")
@@ -56,59 +49,44 @@ pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
 pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
-
-# Functions
 def go_up():
     if head.direction != "down":
         head.direction = "up"
-
 def go_down():
     if head.direction != "up":
         head.direction = "down"
-
 def go_left():
     if head.direction != "right":
         head.direction = "left"
-
 def go_right():
     if head.direction != "left":
         head.direction = "right"
-
 def move():
     if head.direction == "up":
         y = head.ycor()
         head.sety(y + 20)
-
     if head.direction == "down":
         y = head.ycor()
         head.sety(y - 20)
-
     if head.direction == "left":
         x = head.xcor()
         head.setx(x - 20)
-
     if head.direction == "right":
         x = head.xcor()
         head.setx(x + 20)
-
-# Keyboard bindings
 wn.listen()
 wn.onkeypress(go_up, "w")
 wn.onkeypress(go_down, "s")
 wn.onkeypress(go_left, "a")
 wn.onkeypress(go_right, "d")
-
-# Main game loop
 while True:
     wn.update()
-
-    # Check for a collision with the border
     if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
         time.sleep(1)
         head.goto(0,0)
         head.direction = "stop"
 
-        # Hide the segments
+        
         for segment in segments:
             segment.goto(1000, 1000)
         
---
+++ week13/2311.py
@@ -0,0 +1,14 @@
+d= {"dict": {"1":2, "2":4, "data":{"data":[1,2,34]}}}
+a=d["dict"]
+b=a["1"]
+c=a["2"]
+e=a["data"]
+f=e["data"]
+
+print(f)
+print(b)
+print(c)
+
+
+
+
